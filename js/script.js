750073ff0aa7d64d9edd55e1a780bb5cf4b00afa
Imfortes <imfortes@list.ru>
2019-02-19 22:25:02 +0300

    Second push

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+   
     const cartWrapper = document.querySelector('.cart__wrapper'),
         cart = document.querySelector('.cart'),
         close = document.querySelector('.cart__close'),
@@ -31,9 +32,12 @@ window.addEventListener('DOMContentLoaded', () => {
                 empty = cartWrapper.querySelector('.empty');
 
             trigger.remove();
+
+            showConfirm();
+            calcGoods(1);
+
             removeBtn.classList.add('goods__item-remove');
             removeBtn.innerHTML = '&times';
-
             item.appendChild(removeBtn);
 
             cartWrapper.appendChild(item);
@@ -41,8 +45,55 @@ window.addEventListener('DOMContentLoaded', () => {
             if (empty) {
                 empty.remove();
             }
+
+            calcTotal();
         });
     });
+
+
+    function sliceTitle(){
+        titles.forEach(function (item) {
+            if (item.textContent.length < 70){
+                return;
+            } else {
+                // const str = `${item.textContent.slice(0, 71)} ...`;
+                item.textContent = item.textContent.slice(0, 71) + '...';
+            }
+        })
+    }
+    sliceTitle();
+
+    function showConfirm() {
+        confirm.style.display = 'block';
+        let counter = 100;
+        const id = setInterval(frame(), 10);
+        function frame() {
+            if (counter == 10) {
+                clearInterval(id);
+                confirm.style.display = 'none';
+            } else {
+                counter--;
+                confirm.style.opacity = '.' + counter;
+                confirm.style.transform = `translateY(-${counter}px)`;
+            }
+
+        }
+    }
+
+    function calcGoods(i) {
+        const items = cartWrapper.querySelectorAll('.goods__item');
+        badge.textContent = items.length + i;
+    }
+
+    function calcTotal() {
+        const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+        let total = 0;
+        prices.forEach(function (item) {
+            total += +item.textContent;
+        });
+        totalCost.textContent = total;
+    }
+
 });
 
 
